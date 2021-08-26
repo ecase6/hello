2c2b9214a42254e6a7967c5eb7722d03e5dd0c07
Erin Case <ecase6@huskers.unl.edu>
2021-08-26 17:47:56 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Erin Case and Abby Kelly
+ *         
+ * Date: 2021/08/24
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Erin Case and Abby Kelly!\n");
+  printf("Computer Science\n");
+
+  return 0;
+}
